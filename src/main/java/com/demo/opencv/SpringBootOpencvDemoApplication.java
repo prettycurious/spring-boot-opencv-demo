0448f90eafa6d0a0fd5a9af8071078c78a4f07ab
田文渊 <tianwenyuan@suitbim.com>
2024-01-16 17:22:07 +0800

    Initial commit

---
+++ src/main/java/com/demo/opencv/SpringBootOpencvDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.opencv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpencvDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOpencvDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/opencv/SpringBootOpencvDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.opencv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpencvDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
